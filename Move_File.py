7deaf3760d19bfb67922d3b2d3b6c9ea8f66d3bf
Bhuvaneshsoni294 <89522795+Bhuvaneshsoni294@users.noreply.github.com>
2023-02-18 18:57:49 +0530

    Add files via upload

---
+++ Move_File.py
@@ -0,0 +1,7 @@
+import os
+import shutil
+
+path = "C:/Users/hp/Desktop/whitehat jr/C111 Image/feather.jfif"
+root, extension = os.path.splitext(path);
+print("the root element are: ",root);
+print("the extension element are: ",extension);
